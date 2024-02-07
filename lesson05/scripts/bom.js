cde7fcd06534f0e6df92268d5a301d50d5ea9cdf
Mandi <mandibeauchaine1@gmail.com>
2024-02-07 15:31:30 -0700

    notes added

--- lesson05/scripts/bom.js
+++ lesson05/scripts/bom.js
@@ -10,26 +10,35 @@ button.addEventListener('click', () => {
         alert("Must not be blank");
         return false;} 
 
+    /*create a li elemen*/    
     const li = document.createElement('li');
 
+    /*create a delete button*/
     const deleteButton = document.createElement('button');
     
+    /*populate the li elements textContent or innerHTML with the input value*/
     li.textContent = input.value;
 
+    /*populate the button textContent with a ❌*/
     deleteButton.textContent = '❌';
 
-    
+    /*append the li element with the delete button*/
     li.append(deleteButton);
     
-
+    /*append the li element to the unordered list in your HTML*/
     list.append(li);
         
+    /*add an event listener to the delete button that removes the li element when clicked*/
     deleteButton.addEventListener('click', function(){
         list.removeChild(li);
-        
-        input.value = '';
+        input.focus();    
     });
+
+    /*send the focus to the input element*/
     input.focus();
+    
+    /*change the input value to nothing or the empty string to clean up the interface for the user*/
+    input.value = '';
 });
 
 
