868d6bfcc48747d4339b064ba998bc1d21af3692
Mandi <mandibeauchaine1@gmail.com>
2024-02-16 12:25:17 -0700

    fix windchil if else statements

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -18,8 +18,21 @@ function weatherBalloon( cityID ) {
 
   function drawWeather( d ) {
 	var fahrenheit = Math.round(((parseFloat(d.main.temp)-273.15)*1.8)+32); 
-	
-	document.getElementById('temp').innerHTML = 'Current Temp: ' + fahrenheit + '&deg;';
+    var windChill = Math. round(35.74+(.6215*fahrenheit) -(35.75*(d.wind.speed)**.16)+(.4275*fahrenheit*(d.wind.speed)**.16));
+    
+    
+	document.getElementById('temp').innerHTML = 'Current Temp: ' + fahrenheit + '&deg; F';
 	document.getElementById('location').innerHTML =  d.name;
     document.getElementById('windspeed').innerHTML = 'Wind Speed: ' + d.wind.speed;
+    if (fahrenheit <= 50 & d.wind.speed > 3)
+    {document.getElementById('windchill').innerHTML = 'Wind Chill: ' + 'N/A'; 
+
+    }else {
+      document.getElementById('windchill').innerHTML = 'Wind Chill: ' + windChill + '&deg; F';
+    }
+  
+    
 }
+
+
+
