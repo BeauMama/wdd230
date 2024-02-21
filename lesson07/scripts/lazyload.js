82317356fef2785d2bb07b6825e5385abf2f00e4
Mandi <mandibeauchaine1@gmail.com>
2024-02-21 15:58:34 -0700

    lazy loading

---
+++ lesson07/scripts/lazyload.js
@@ -0,0 +1,5 @@
+var oLastModif = new Date(document.lastModified).toLocaleString();
+document.getElementById('lastModified').innerHTML = `Last Modified ${oLastModif} `;
+
+var copyrightYear = new Date().getFullYear();
+document.getElementById('copyright').innerHTML = `&copy${copyrightYear} `;
