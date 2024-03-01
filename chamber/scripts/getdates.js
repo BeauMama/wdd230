aef20d78bd18ba2fe61da9095a4bf6a0af1180a7
Mandi <mandibeauchaine1@gmail.com>
2024-02-29 23:47:22 -0700

    upload join.html information

--- chamber/scripts/getdates.js
+++ chamber/scripts/getdates.js
@@ -5,3 +5,11 @@ document.getElementById('lastModified').innerHTML = `Last Modified ${oLastModif}
 var copyrightYear = new Date().getFullYear();
 document.getElementById('copyright').innerHTML = `&copy${copyrightYear} `;
 
+function setLoadTime() {
+    var loadTime = document.getElementById('loaddate');
+    if (loadTime){
+        loadTime.value = Date.now();
+    }
+}
+
+document.getElementById("#join_page").addEventListener('submit',setLoadTime);
