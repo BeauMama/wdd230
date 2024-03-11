19ee1d10755972ebf598ca1df0fbd0283e4237db
Mandi <mandibeauchaine1@gmail.com>
2024-03-11 14:48:34 -0600

    update weather on main index.html page

--- scripts/weather.js
+++ scripts/weather.js
@@ -1,4 +1,4 @@
-
+const weatherIcon = document.querySelector('#weather-icon');
 
 function weatherBalloon( cityID ) {
     var key = '5e83879f6a3a3c4e00ff698e049f9146';
@@ -19,10 +19,16 @@ function weatherBalloon( cityID ) {
   function drawWeather( d ) {
 	var fahrenheit = Math.round(((parseFloat(d.main.temp)-273.15)*1.8)+32); 
     var windChill = Math. round(35.74+(.6215*fahrenheit) -(35.75*(d.wind.speed)**.16)+(.4275*fahrenheit*(d.wind.speed)**.16));
+    var desc = d.weather[0].description;
+	var iconsrc = `https://openweathermap.org/img/wn/${d.weather[0].icon}@2x.png`;
+   
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', 'weather imag');
     
-    
-	document.getElementById('temp').innerHTML = 'Temp: ' + fahrenheit + '&deg; F';
-	document.getElementById('location').innerHTML =  d.name;
+	document.getElementById('temp').innerHTML = fahrenheit + '&deg; F';
+	document.getElementById('desc').innerHTML = desc;
+	document.getElementById('location').innerHTML = 'Weather For: ' + d.name;
+
   
     
 }
